d146ca684e12549e965875ab3dde84e820e12698
Lesley van der Lee <l.lee@tayo.ws>
2020-03-08 10:09:34 +0100

    Implement simple non-functional entrypoint

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    int dirs = 0;
+    int files = 0;
+
+    for (const auto &entry : fs::directory_iterator("./"))
+    {
+        if (entry.is_directory())
+        {
+            dirs++;
+        }
+        else
+        {
+            files++;
+        }
+
+        auto name = entry.path().filename();
+
+        std::cout << name.string() << std::endl;
+    }
+
+    std::cout << std::endl
+              << dirs << " directories, " << files << " files" << std::endl;
+}

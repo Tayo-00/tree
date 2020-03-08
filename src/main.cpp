82db53f5f8421666f7d2999460e827881b251b94
Lesley van der Lee <l.lee@tayo.ws>
2020-03-08 11:24:03 +0100

    Rework entrypoint to use node class

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
+
+#include "node.hpp"
+
 namespace fs = std::filesystem;
 
 int main() {
     int dirs = 0;
     int files = 0;
 
+    std::cout << "." << std::endl;
+
     for (const auto& entry : fs::directory_iterator("./")) {
-        if (entry.is_directory()) {
-            dirs++;
-        } else {
-            files++;
-        }
-
-        auto name = entry.path().filename();
-
-        std::cout << name.string() << std::endl;
+        tree::node(entry, &files, &dirs, 1);
     }
 
     std::cout << std::endl << dirs << " directories, " << files << " files" << std::endl;

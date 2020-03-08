feacb0d21f37dfb3a0718bae44d55b566424e2e1
Lesley van der Lee <l.lee@tayo.ws>
2020-03-08 19:08:27 +0100

    Output doesn't have to be 1:1

--- src/sort.cpp
+++ src/sort.cpp
@@ -10,9 +10,5 @@ bool tree::sort::alphabetical(const fs::directory_entry& a, const fs::directory_
     auto one = (a.path().filename()).generic_string();
     auto two = (b.path().filename()).generic_string();
 
-    // TODO: Reduces performance by ~10-15%
-    std::transform(one.begin(), one.end(), one.begin(), ::toupper);
-    std::transform(two.begin(), two.end(), two.begin(), ::toupper);
-
     return (strcoll(one.c_str(), two.c_str()) < 0);
 }

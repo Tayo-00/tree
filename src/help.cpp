a9ba9aa2691529e8fc6339d6ff0ad0ab62b9a29b
Lesley van der Lee <l.lee@tayo.ws>
2020-03-13 21:14:51 +0100

    Expand usage with -a

--- src/help.cpp
+++ src/help.cpp
@@ -4,5 +4,8 @@
 
 void tree::help::print() {
     std::cout << "usage: tr++ [-a]" << std::endl;
-    std::cout << "        [<directory list>]" << std::endl;
+    std::cout << "        [<directory list>]" << std::endl << std::endl;
+
+    std::cout << "listing options:" << std::endl;
+    std::cout << "  -a                       All files are listed" << std::endl;
 }

e85d528514d4c29bb90503a980dfaebda17ce50f
Lesley van der Lee <l.lee@tayo.ws>
2020-03-14 09:03:46 +0100

    Add multiple directory input parsing

--- include/options.hpp
+++ include/options.hpp
@@ -1,6 +1,9 @@
 #ifndef OPTIONS_HPP
 #define OPTIONS_HPP
 
+#include <string>
+#include <vector>
+
 namespace tree {
     class options {
       public:
@@ -8,6 +11,7 @@ namespace tree {
         static bool all_files;
         static bool directories_only;
         static bool show_help;
+        static std::vector<std::string> directories;
     };
 }  // namespace tree
 
--- src/options.cpp
+++ src/options.cpp
@@ -6,12 +6,16 @@
 bool tree::options::show_help = false;
 bool tree::options::all_files = false;
 bool tree::options::directories_only = false;
+std::vector<std::string> tree::options::directories = {"."};
 
 void tree::options::parse(int argc, char* argv[]) {
-    std::string arg1 = std::string(argv[1]);
+    std::string arg1 = argv[1];
+
+    int i = 1;
 
     if (arg1.substr(0, 2) == "--" && arg1 == "--help") {
         tree::options::show_help = true;
+        i++;
     }
 
     if (arg1.substr(0, 2) != "--" && arg1.substr(0, 1) == "-") {
@@ -22,5 +26,33 @@ void tree::options::parse(int argc, char* argv[]) {
         if (arg1.find('d') != std::string::npos) {
             tree::options::directories_only = true;
         }
+
+        i++;
+    }
+
+    while (i < argc) {
+        std::string arg2 = argv[i];
+
+        if (arg2.substr(0, 2) == "--") {
+        }
+
+        if (arg2.substr(0, 2) != "--" && arg2.substr(0, 1) == "-") {
+        }
+
+        if (arg2.substr(0, 2) != "--" && arg2.substr(0, 1) != "-") {
+            break;
+        }
+
+        i++;
+    }
+
+    if (i < argc) {
+        tree::options::directories = {};
+    }
+
+    while (i < argc) {
+        tree::options::directories.push_back(argv[i]);
+
+        i++;
     }
 }

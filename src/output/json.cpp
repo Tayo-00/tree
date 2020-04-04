da7f29c4c98ace19682aeb1de1ea7d070172fbe0
Lesley van der Lee <l.lee@tayo.ws>
2020-04-04 11:05:34 +0200

    Implement JSON symlink handling

--- src/output/json.cpp
+++ src/output/json.cpp
@@ -6,8 +6,8 @@
 namespace fs = std::filesystem;
 const std::string tab = "  ";
 
-inline void indent(int level) {
-    while (level--) std::cout << tab;
+inline void indent(int level, std::string* line) {
+    while (level--) (*line) += tab;
 }
 
 void tree::serializers::json::initially_open() {
@@ -18,21 +18,38 @@ void tree::serializers::json::print(std::string* prefix, bool last, int depth,
                                     std::filesystem::directory_entry path,
                                     std::string override_name = "") {
     std::string line = *prefix;
-    indent(depth + 1);
+    indent(depth + 1, &line);
 
-    std::string name = path.path().filename();
+    std::string name = "";
+
+    if (path.is_symlink()) {
+        auto resolved = fs::directory_entry(fs::read_symlink(path));
+        name = path.path().filename().string();
+    } else {
+        name = path.path().filename().string();
+    }
 
     if (depth == 0) {
         name = override_name;
     }
 
-    if (path.is_directory()) {
-        std::cout << "{\"type\":\"directory\", \"name\": \"" << name << "\", \"contents\": [";
-    } else {
-        std::cout << "{\"type\":\"file\", \"name\": \"" << name << "\"}";
+    if (path.is_symlink()) {
+        auto resolved = fs::directory_entry(fs::read_symlink(path));
+        line += "{\"type\": \"link\", \"name\": \"" + name + "\", \"target\": \"" +
+                resolved.path().string() + "\", \"contents\": []}";
 
         if (!last) {
-            std::cout << ",";
+            line += ",";
+        }
+    } else {
+        if (path.is_directory()) {
+            line += "{\"type\": \"directory\", \"name\": \"" + name + "\", \"contents\": [";
+        } else {
+            line += "{\"type\": \"file\", \"name\": \"" + name + "\"}";
+
+            if (!last) {
+                line += ",";
+            }
         }
     }
 
@@ -40,23 +57,26 @@ void tree::serializers::json::print(std::string* prefix, bool last, int depth,
 }
 
 void tree::serializers::json::close_entry(bool last, int depth) {
-    indent(depth + 1);
+    std::string line = "";
+    indent(depth + 1, &line);
     if (last == true && depth > 0) {
-        std::cout << "]}";
+        line += "]}\n";
     } else {
-        std::cout << "]},";
+        line += "]},\n";
     }
 
-    std::cout << '\n';
+    std::cout << line;
 }
 
 void tree::serializers::json::print_statistics(bool directories_only, int dirs, int files) {
-    indent(1);
-    std::cout << "{\"type\": \"report\", \"directories\": " << dirs;
-    if (!directories_only == true) {
-        std::cout << ", \"files\": " << files;
+    std::string line = "";
+    indent(1, &line);
+    if (directories_only == true) {
+        std::cout << line << "{\"type\": \"report\", \"directories\": " << dirs << "}\n";
+    } else {
+        std::cout << line << "{\"type\": \"report\", \"directories\": " << dirs
+                  << ", \"files\": " << files << "}\n";
     }
-    std::cout << "}\n";
 }
 
 void tree::serializers::json::finally_close() {

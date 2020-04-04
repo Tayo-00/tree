ce77b284f24edc48618fde5e0645632fd4fee123
Lesley van der Lee <l.lee@tayo.ws>
2020-04-04 11:23:18 +0200

    Update usage information

--- src/help.cpp
+++ src/help.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 
 void tree::help::print() {
-    std::cout << "usage: tr++ [-a]" << std::endl;
-    std::cout << "        [<directory list>]" << std::endl << std::endl;
+    std::cout << "usage: tr++ [-adJ]" << std::endl;
+    std::cout << "        [<directory list>]" << std::endl;
 
-    std::cout << "listing options:" << std::endl;
+    std::cout << std::endl << "listing options:" << std::endl;
     std::cout << "  -a                       All files are listed" << std::endl;
     std::cout << "  -d                       List directories only" << std::endl;
+    std::cout << "  -J                       Print output as JSON" << std::endl;
+
+    std::cout << std::endl << "miscellaneous options:" << std::endl;
+    std::cout << "  --help                   Print this help message" << std::endl;
 }

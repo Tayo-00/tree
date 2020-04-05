4353e39132369a96e017bf215a36207e0397216b
Lesley van der Lee <l.lee@tayo.ws>
2020-04-05 09:35:08 +0200

    Update help with new parameters

--- src/help.cpp
+++ src/help.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 
 void tree::help::print() {
-    std::cout << "usage: tr++ [-adJ]" << std::endl;
+    std::cout << "usage: tr++ [-adJ] [-L level]" << std::endl;
     std::cout << "        [<directory list>]" << std::endl;
 
     std::cout << std::endl << "listing options:" << std::endl;
     std::cout << "  -a                       All files are listed" << std::endl;
     std::cout << "  -d                       List directories only" << std::endl;
+    std::cout << "  -L level                 Only descend level of directories deep" << std::endl;
+
+    std::cout << std::endl << "output options:" << std::endl;
     std::cout << "  -J                       Print output as JSON" << std::endl;
 
     std::cout << std::endl << "miscellaneous options:" << std::endl;

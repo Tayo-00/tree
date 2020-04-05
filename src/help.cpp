fde82a7edb89ff9bc7b13ec0d2e48ea062edf1ff
Lesley van der Lee <l.lee@tayo.ws>
2020-04-05 10:00:50 +0200

    Add no indent option to help

--- src/help.cpp
+++ src/help.cpp
@@ -14,6 +14,9 @@ void tree::help::print() {
     std::cout << std::endl << "output options:" << std::endl;
     std::cout << "  -J                       Print output as JSON" << std::endl;
 
+    std::cout << std::endl << "graphics options:" << std::endl;
+    std::cout << "  -i                       Don't print indentation lines" << std::endl;
+
     std::cout << std::endl << "miscellaneous options:" << std::endl;
     std::cout << "  --help                   Print this help message" << std::endl;
 }
